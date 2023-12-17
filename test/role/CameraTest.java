da92df7827c824f23c8b89a0485bf64a6f8bf327
lin-roger <roger920707fb@gmail.com>
2023-12-17 11:38:54 +0800

    test

---
+++ test/role/CameraTest.java
@@ -0,0 +1,31 @@
+package role;
+
+import junit.framework.TestCase;
+
+public class CameraTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testGetMinX() {
+    }
+
+    public void testGetMaxX() {
+    }
+
+    public void testGetMinY() {
+    }
+
+    public void testGetMaxY() {
+    }
+
+    public void testFollowUnit() {
+    }
+}
